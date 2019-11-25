718fd8c7360ef729f7313dbac3b2d787ff79fa36
frOstzz <luck123444@gmail.com>
2019-11-25 18:01:56 +0300

    add file mainPage.js

---
+++ scripts/modules/mainPage.js
@@ -0,0 +1,41 @@
+import {
+  createButtons,
+  publicateButton,
+  createContentEditableDiv,
+  createHashForDiv,
+  createDivs,
+} from '../modules/createElements.js';
+
+export const createMainPage = () => {
+  const mainDiv = createDivs(createHashForDiv('container', 'container'));
+  const editor = createContentEditableDiv(createHashForDiv('editor', 'editor'));
+  const information = createDivs(createHashForDiv('', 'information'));
+  const stateList = createDivs(createHashForDiv('state-list', 'state-list'));
+  const personInfo = createDivs(createHashForDiv('', 'person-info'));
+  const combineDiv = createDivs(createHashForDiv('', 'combine-div'));
+
+  //times code==============
+  const username = 'Admin';
+  const states = '3';
+
+  personInfo.innerHTML = `<span class="info-text">Your name: <strong>${username}</strong></span>
+                              <span class="info-text">Your states: <strong>${states}</strong></span>`;
+  //============================
+
+  //buttons
+  const addArticle = createButtons(publicateButton);
+
+  //Append DOM elements
+  document.body.appendChild(mainDiv);
+  mainDiv.appendChild(combineDiv);
+
+  combineDiv.appendChild(stateList);
+
+  combineDiv.appendChild(information);
+  information.appendChild(personInfo);
+  personInfo.appendChild(addArticle);
+
+  addArticle.addEventListener('click', () => {
+    mainDiv.remove(combineDiv);
+  });
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,6 @@
-import {
-  createButtons,
-  publicateButton,
-  createContentEditableDiv,
-  createHashForDiv,
-  createDivs,
-} from '../scripts/modules/createElements.js';
-
+import { createMainPage } from './modules/mainPage.js';
 const main = () => {
-  //divs
-  const mainDiv = createDivs(createHashForDiv('container', 'container'));
-  const editor = createContentEditableDiv(createHashForDiv('editor', 'editor'));
-  const information = createDivs(createHashForDiv('', 'information'));
-  const stateList = createDivs(createHashForDiv('state-list', 'state-list'));
-  const personInfo = createDivs(createHashForDiv('', 'person-info'));
-
-  //times code==============
-  const username = 'Admin';
-  const states = '3';
-
-  personInfo.innerHTML = `<span class="info-text">Your name: <strong>${username}</strong></span>
-                          <span class="info-text">Your states: <strong>${states}</strong></span>`;
-  //============================
-
-  //buttons
-  const addArticle = createButtons(publicateButton);
-
-  document.body.appendChild(mainDiv);
-  //mainDiv.appendChild(editor); Contenteditable div
-
-  mainDiv.appendChild(stateList);
-
-  mainDiv.appendChild(information);
-  information.appendChild(personInfo);
-  personInfo.appendChild(addArticle);
+  createMainPage();
 };
 
 main();
