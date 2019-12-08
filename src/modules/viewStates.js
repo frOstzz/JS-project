afc4e3c70ae9ae46f040bc59853136403ee996c7
frOstzz <luck123444@gmail.com>
2019-12-08 21:49:54 +0300

    added view state (bug)

--- src/modules/viewStates.js
+++ src/modules/viewStates.js
@@ -1,19 +1,31 @@
 import { arrForState } from './storage.js';
+import { createDivs, createHashForDiv, createButtons, backButton } from './createElements.js';
 
 export const viewState = () => {
   const mainDiv = document.getElementById('container');
   const combDiv = document.getElementById('combine-div');
+  const backBtn = createButtons(backButton);
 
   combDiv.onclick = (event) => {
     let target = event.target;
     if (target.className === 'custom-article') {
       combDiv.classList.remove('combine-div');
       combDiv.classList.add('hide');
-      const divchik = document.createElement('div');
-      mainDiv.appendChild(divchik);
 
       for (let i = 0; i < arrForState.length; i += 1) {
-        divchik.innerHTML = arrForState[i].state;
+        const viewStates = createDivs(createHashForDiv('view-state' + i, 'view'));
+
+        const backToMainPage = () => {
+          viewStates.remove();
+          combDiv.classList.remove('hide');
+          combDiv.classList.add('combine-div');
+        };
+
+        mainDiv.appendChild(viewStates);
+        viewStates.innerHTML = arrForState[i].state;
+        viewStates.appendChild(backBtn);
+
+        backBtn.addEventListener('click', backToMainPage);
       }
     }
   };
