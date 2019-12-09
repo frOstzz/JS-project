78e721f7e097216a71acde40fb1369682439ae43
frOstzz <luck123444@gmail.com>
2019-12-09 14:07:47 +0300

    tried fixed bug with delete articles, but it's failed

--- src/modules/deleteState.js
+++ src/modules/deleteState.js
@@ -2,15 +2,19 @@ import { stateIds, delButtonsIds } from './storage.js';
 
 export const deleteState = () => {
   const allDeleteButtons = document.getElementsByClassName('delete-button');
-  const stateId = stateIds.findIndex((el) => el === event.target.id);
 
   const delState = () => {
     const stateId = stateIds.findIndex((el) => el === event.target.id);
     stateIds.splice(stateId, 1);
-    //delButtonsIds.splice(stateId, 1);
-    const stateList = document.getElementsByClassName('state-list');
-    let state = document.getElementById('state' + event.target.id);
-    stateList.remove(state);
+    delButtonsIds.splice(stateId, 1);
+    console.log(stateIds);
+    console.log(delButtonsIds);
+
+    const stateList = document.getElementById('state-list');
+    let state = document.getElementById('state' + stateIds.length);
+    console.log(stateIds.length);
+    console.log(state);
+    state.remove();
   };
 
   for (let i = 0; i < allDeleteButtons.length; i += 1) {
