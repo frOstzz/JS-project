884883f5b62b77d95e39a241a34a06bdacf5a1b0
frOstzz <luck123444@gmail.com>
2019-12-11 14:54:58 +0300

    added validation for a title in the editor

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -1,1 +1,31 @@
-//comming soon ;)
+export const titleValid = () => {
+  const articleTitle = document.getElementById('title');
+  const pubBtn = document.getElementById('pub');
+
+  if (articleTitle.value === '') {
+    articleTitle.classList.remove('process');
+    articleTitle.classList.remove('valid');
+    articleTitle.classList.add('invalid');
+    pubBtn.disabled = true;
+  } else if (articleTitle.value.length > 50) {
+    articleTitle.classList.remove('process');
+    articleTitle.classList.remove('valid');
+    articleTitle.classList.add('invalid');
+    pubBtn.disabled = true;
+  } else if (articleTitle.value.length < 10) {
+    articleTitle.classList.remove('process');
+    articleTitle.classList.remove('valid');
+    articleTitle.classList.add('invalid');
+    pubBtn.disabled = true;
+  } else {
+    articleTitle.classList.remove('process');
+    articleTitle.classList.add('valid');
+    pubBtn.disabled = false;
+  }
+};
+
+export const inProcess = () => {
+  const articleTitle = document.getElementById('title');
+
+  articleTitle.classList.add('process');
+};
